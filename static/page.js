f14016c05ecf1586de743fd3a05f87a6747e4f71
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-04-04 19:26:32 -0400

    Initial commit

---
+++ static/page.js
@@ -0,0 +1,67 @@
+const fUtil = require("../misc/file");
+const stuff = require("./info");
+const http = require("http");
+
+function toAttrString(table) {
+	return typeof table == "object"
+		? Object.keys(table)
+				.filter((key) => table[key] !== null)
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`)
+				.join("&")
+		: table.replace(/"/g, '\\"');
+}
+function toParamString(table) {
+	return Object.keys(table)
+		.map((key) => `<param name="${key}" value="${toAttrString(table[key])}">`)
+		.join(" ");
+}
+function toObjectString(attrs, params) {
+	return `<object id="obj" ${Object.keys(attrs)
+		.map((key) => `${key}="${attrs[key].replace(/"/g, '\\"')}"`)
+		.join(" ")}>${toParamString(params)}</object>`;
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "GET") return;
+	const query = url.query;
+
+	var attrs, params, title;
+	switch (url.pathname) {
+                  case "/player": {
+			title = "Player";
+			attrs = {
+				data: process.env.SWF_URL + "/player.swf",
+				type: "application/x-shockwave-flash",
+				width: "100%",
+				height: "100%",
+			};
+			params = {
+				flashvars: {
+	apiserver: "/", ctc: "go", tlang: "en_US",
+        autostart: "1", appCode: "go", isEmbed: "0", 
+	storePath: process.env.STORE_URL + "/<store>", 
+	clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+    },
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+default:
+			return;
+	}
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
+	Object.assign(params.flashvars, query);
+	res.end(
+		`<script>document.title='${title}',flashvars=${JSON.stringify(
+			params.flashvars
+		)}</script><body style="margin:0px">${toObjectString(attrs, params)}</body>${stuff.pages[url.pathname] || ""}`
+	);
+	return true;
+};
